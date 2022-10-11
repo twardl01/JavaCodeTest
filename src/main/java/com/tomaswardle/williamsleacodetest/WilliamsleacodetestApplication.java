db7f7d85ad538a7ea6716acb7ccd586f67b0fcc0
twardl01 <81767595+twardl01@users.noreply.github.com>
2022-10-11 11:05:06 +0100

    first commit

---
+++ src/main/java/com/tomaswardle/williamsleacodetest/WilliamsleacodetestApplication.java
@@ -0,0 +1,13 @@
+package com.tomaswardle.williamsleacodetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilliamsleacodetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilliamsleacodetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomaswardle/williamsleacodetest/WilliamsleacodetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomaswardle.williamsleacodetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilliamsleacodetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
